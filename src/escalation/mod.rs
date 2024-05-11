55f443386645cc5a1591acc11e5b703218ba910c
Astrid Yu <astrid@astrid.tech>
2024-05-10 19:47:51 -0700

    Revert "fix terminal corruption wip"
    This reverts commit 7b07427eed97e047262ee25ddd3aca9b726cf346.

--- src/escalation/mod.rs
+++ src/escalation/mod.rs
@@ -3,14 +3,7 @@
 mod darwin;
 mod unix;
 
-use std::process::Stdio;
-
 pub use self::unix::Command;
-
-/// A token that is used to detect if a process has been escalated.
-///
-/// If the child process spits this out on stdout that means we escalated successfully.
-pub const SUCCESS_TOKEN: &'static str = "znxbnvm,,xbnzcvnmxzv,.,,,";
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
@@ -26,32 +19,19 @@ pub async fn run_escalate(
     cmd: &Command<'_>,
     modify: impl FnOnce(&mut tokio::process::Command) -> (),
 ) -> anyhow::Result<tokio::process::Child> {
-    use self::unix::EscalationMethod;
+    #[cfg(target_os = "linux")]
+    {
+        use self::unix::EscalationMethod;
 
-    let mut cmd: tokio::process::Command = EscalationMethod::detect()?.wrap_command(cmd).into();
-    cmd.stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped());
-    modify(&mut cmd);
+        let mut cmd: tokio::process::Command = EscalationMethod::detect()?.wrap_command(cmd).into();
+        modify(&mut cmd);
+        Ok(cmd.spawn()?)
+    }
 
-    let proc = cmd.spawn()?;
-    let stderr = proc.stdout.take().unwrap();
-    let stdin = proc.stdin.take().unwrap();
+    #[cfg(target_os = "macos")]
+    {
+        use self::darwin::wrap_osascript_escalation;
 
-    tokio::spawn(async move {
-        let stdout = proc.stdout.take().unwrap();
-    });
-    Ok()
-}
-
-struct EscalationInProgress {
-    _child: tokio::process::Child,
-    listener: JoinHandle<bool>,
-}
-
-impl EscalationInProgress {
-    pub fn await_escalation(self) -> anyhow::Result<()> {
-        self.listener.await?;
-        Ok(())
+        wrap_osascript_escalation(cmd, modify).await
     }
 }
